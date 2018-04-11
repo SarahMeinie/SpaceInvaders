fad22deb3c795c0d79915a8cc6abe21dca0cdb3a
19827784 <38248747+19827784@users.noreply.github.com>
2018-04-11 17:26:02 +0200

    Add files via upload

--- DefaultCritter.java
+++ DefaultCritter.java
@@ -1,5 +1,47 @@
-/*
- * Imeplemts default metgids if the Critter API
- * */
-public class DefaultCritter{
-}
+public class DefaultCritter implements critter {
+	private double x;
+	private double y;
+	private double vx;
+	private double vy;
+	private int radius;
+	private int health;
+	private double dx;
+	private double theta;
+	private double deg;
+	private double min_x;
+	private double max_x;
+	private double min_angle;
+	private double max_angle;
+	
+  public void setPosition(int x, int y){
+	  this.x = x;
+	  this.y = y
+  }
+ 
+  //do we need getters?
+
+  public void setVelocity(int vx, int vy){
+	  this.vx = vx;
+	  this.vy = vy;
+  }
+  
+  public double get_theta(){
+	  return theta;
+  }
+ 
+  public double get_x(){
+	  return x;
+  }
+  
+  public void set_theta(double theta){
+	  this.theta = theta;
+	  if (theta < min_angle){
+		  this.theta = min_angle;
+	  }
+	  if (theta > max_angle){
+		  this.theta = max_angle;
+	  }
+  	}
+ 
+  }
+  
