b3cd6a98920df83f89453d5dd639a18901e2b7b2
19827784 <38248747+19827784@users.noreply.github.com>
2018-04-11 17:32:13 +0200

    Add files via upload

--- Critter.java
+++ Critter.java
@@ -1,5 +1,15 @@
-/*
- * Specifies minimum API for in-game objects
- * */
-public interface Critter{
+
+public interface Critter {
+ 
+ public void setPosition(double x, double y);
+ public void setVelocity(double vx, double vy);
+ public double get_VelocityY();
+ public double get_VelocityX();
+ public double get_theta();
+ public double get_x();
+ public void set_theta(double theta);
+ public void rotate_anti();
+ public void rotate_clock();
+ public void move_right();
+ public void move_left();
 }
--- Shooter.java
+++ Shooter.java
@@ -1,74 +1,13 @@
 public class Shooter extends DefaultCritter{
-    //needs to override default functionality 
-  private double dx  = 0.02;
-  private double x   = 0.5;
-  private double y   = 0.1;
-  private double deg = 5;
-  private double theta = 0;
-  private double min_angle=-90;
-  private double max_angle=90;
-  private double min_x=0.05;
-  private double max_x=0.95;
-  private double h=0.1;
-  private double w=0.1;
-  
-  
-  public double get_theta(){
-  return theta;
- }
+ private double y   = 0.1;
+ private double h=0.1;
+ private double w=0.1;
  
- public double get_x(){
-  return x;
- }
- 
-
   public void draw_shooter(){
-      this.x = x;
+      //this.x = x;
       StdDraw.clear(StdDraw.BLACK);
-      StdDraw.picture(x, y, "shooter.png", w, h, theta);
+      StdDraw.picture(get_x(), y, "shooter.png", w, h, get_theta());
       StdDraw.show(20);
   }
-  
-   public void set_theta(double theta)
- {
-  this.theta = theta;
-  if (theta < min_angle){
-   this.theta = min_angle;
-  }
-  if (theta > max_angle){
-   this.theta = max_angle;
-  }
- }
-
-public void move_left(){
-  x -= dx;
-  if (x < min_x){
-   x = min_x;
-  }
- }
-
- public void move_right(){
-  x += dx;
-  if (x > max_x){
-   x = max_x;
-  }
- }
-
- public void rotate_anti(){
- theta = theta - deg;
-  if (theta < min_angle){
-   theta = min_angle;
-  }
- }
-
- public void rotate_clock(){
-   theta = theta + deg;
-  if (theta > max_angle){
-   theta = max_angle;
-  }
- }
- 
- 
-
 
 }
