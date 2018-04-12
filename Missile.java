6560f6f3887045907d6c37baa66477fe51d0c70f
19827784 <38248747+19827784@users.noreply.github.com>
2018-04-12 17:21:56 +0200

    Add files via upload

--- Missile.java
+++ Missile.java
@@ -1,3 +1,18 @@
 public class Missile extends DefaultCritter{
-  //needs to override default functionality 
+   private double h=0.1;
+ private double w=0.1;
+ private double x; 
+ private double y;
+ 
+ public Missile(double xx, double yy){
+   this.x = xx;
+   this.y = yy;
+ }
+ 
+  public void draw(){
+      //this.x = x;
+      StdDraw.clear(StdDraw.BLACK);
+      StdDraw.picture(x, y, "bullet.png", w, h, get_theta());
+      StdDraw.show(20);
+  }
 }
