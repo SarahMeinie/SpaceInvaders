3e9375163e65bff98ee1d70df261a339f00a219b
19827784 <38248747+19827784@users.noreply.github.com>
2018-04-15 16:21:06 +0200

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -5,16 +5,19 @@ import java.awt.Color;
 public class Menu{
 
   public void MenuSetUp(){
-    StdDraw.clear();
+    StdDraw.pause(30);
+    StdDraw.picture(0.5, 0.5, "stars.jpg", 1, 1);
     StdDraw.setPenRadius(0.095);
-    StdDraw.setPenColor(StdDraw.BLUE);
+    StdDraw.setPenColor(StdDraw.BLACK);
     StdDraw.point(0.5, 0.5);
     StdDraw.line(0.05, 0.21, 0.95, 0.21);
     Font font = new Font("Arial", Font.BOLD, 40);
     StdDraw.setFont(font);
+    StdDraw.setPenColor(StdDraw.WHITE);
     StdDraw.textLeft(0.25, 0.8, "{Game Name}");
     StdDraw.setPenColor(StdDraw.WHITE);
     StdDraw.textLeft(0.05, 0.2, "Press Spacebar To Play");
+    StdDraw.show();
   }
   
     
