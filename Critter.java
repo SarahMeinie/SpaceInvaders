8abf62219e664d6e46b63cb475ede475f0332595
19827784 <38248747+19827784@users.noreply.github.com>
2018-04-18 10:07:46 +0200

    Add files via upload

--- Critter.java
+++ Critter.java
@@ -1,12 +1,12 @@
 
 public interface Critter {
  
- public void setPosition(double x, double y);
- public void setVelocity(double vx, double vy);
- public double get_VelocityY();
- public double get_VelocityX();
+ public void set_VelocityX(double velocityx);
+ public void set_VelocityY(double velocityy);
  public double get_theta();
  public double get_x();
+ public void set_y(double posy);
+ public void set_x(double posx);
  public void set_theta(double theta);
  public void rotate_anti();
  public void rotate_clock();
