fbf51cb8842b05d28b6ce8c78e61de718a9ed705
19827784 <38248747+19827784@users.noreply.github.com>
2018-04-25 16:32:20 +0200

    Add files via upload

--- Missile.java
+++ Missile.java
@@ -8,17 +8,42 @@ public class Missile extends DefaultCritter{
     vx = -vx;
     vy = -vy;
   }
-  
+
   public void move(){
     y = y + vy;
     x = x + vx;
     draw();
   }
-  
+  public void move1(){
+    y = y + vy;
+    x = x + vx;
+    draw1();
+  }
+  public void move2(){
+    y = y + vy;
+    x = x + vx;
+    draw2();
+  }
+    public void move_enemy_missile(){
+    y = y + vy;
+    draw_enemy_missile();
+  }
   
   public void draw(){
+    StdDraw.picture(x, y, "laser.png", 0.1, 0.1, theta);
+  }
+  
+  public void draw1(){
     StdDraw.setPenColor(StdDraw.WHITE);
     StdDraw.filledCircle(x, y, 0.01);
   }
   
+  public void draw2(){
+    
+    StdDraw.setPenColor(StdDraw.BLUE);
+    StdDraw.filledCircle(x, y, 0.01);
+  }
+  public void draw_enemy_missile(){
+    StdDraw.picture(x, y, "enemy_draw.png", 0.05, 0.05, theta);
+  }
 }
